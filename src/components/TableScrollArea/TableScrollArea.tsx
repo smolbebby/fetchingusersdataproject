2084a6190ba8efd8259cac4668fa6018ecdb8762
smolbebby <125963415+smolbebby@users.noreply.github.com>
2024-02-10 20:09:57 +1000

    added UI components, added function fetchData

---
+++ src/components/TableScrollArea/TableScrollArea.tsx
@@ -0,0 +1,40 @@
+import cx from 'clsx';
+import { useState } from 'react';
+import { Table, ScrollArea } from '@mantine/core';
+import classes from './TableScrollArea.module.css';
+import fetchData from '../fetchData';
+
+const data = [
+  {
+    name: 'Athena Weissnat',
+    company: 'Little - Rippin',
+    email: 'Elouise.Prohaska@yahoo.com',
+  }
+];
+
+export function TableScrollArea() {
+  const [scrolled, setScrolled] = useState(false);
+
+  const rows = data.map((row) => (
+    <Table.Tr key={row.name}>
+      <Table.Td>{row.name}</Table.Td>
+      <Table.Td>{row.email}</Table.Td>
+      <Table.Td>{row.company}</Table.Td>
+    </Table.Tr>
+  ));
+
+  return (
+    <ScrollArea h={300} onScrollPositionChange={({ y }) => setScrolled(y !== 0)}>
+      <Table miw={700}>
+        <Table.Thead className={cx(classes.header, { [classes.scrolled]: scrolled })}>
+          <Table.Tr>
+            <Table.Th>Name</Table.Th>
+            <Table.Th>Email</Table.Th>
+            <Table.Th>Company</Table.Th>
+          </Table.Tr>
+        </Table.Thead>
+        <Table.Tbody>{rows}</Table.Tbody>
+      </Table>
+    </ScrollArea>
+  );
+}
--- src/components/fetchData.tsx
+++ src/components/fetchData.tsx
@@ -1,4 +1,4 @@
-
+import { useState, useEffect } from 'react';
 /**
  * declare fetch function
  * 
@@ -7,3 +7,34 @@
  * 
  * export fetchData
  */
+const url = 'https://dummyjson.com/users';
+
+interface User {
+    id:number,
+    firstName:string,
+    lastName:string,
+    age:number,
+    height:number,
+    hair:string
+}
+
+export default function users() {
+    const [isLoading, setIsLoading] = useState(false);
+    const [users, setUsers] = useState<User[]>([]);
+
+    useEffect(() => {
+        const fetchUsers = async () => {
+            setIsLoading(true);
+            const response = await fetch(`${url}`);
+            const users = await response.json() as { users: User[] };
+            setUsers(users.users);
+            setIsLoading(false);
+        };
+
+        fetchUsers();
+    }, []);
+
+    if (isLoading) {
+        return <div>Loading...</div>
+    }
+}
