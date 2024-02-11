88802f0a0b27cf73f92c70e18416390973027987
smolbebby <125963415+smolbebby@users.noreply.github.com>
2024-02-11 20:28:25 +1000

    created tallestheight/tallestuser function and added into app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ import useUsers from './components/fetchData';
 import AverageAge from './components/averageAge';
 import { TableScrollArea } from './components/TableScrollArea/TableScrollArea';
 import React, { useState, useEffect } from 'react';
+import TallestUser from './components/tallestUser';
 
 export default function App() {
   const { users, isLoading } = useUsers();
   const { averageAge } = AverageAge();
+  const { tallestHeight } = TallestUser();
 
   return (
     <MantineProvider theme={theme}>
@@ -19,6 +21,7 @@ export default function App() {
         ) : (
         < TableScrollArea data={users}/>)}
         <p>Average Age: {averageAge}</p>
+        <p>Tallest User: {tallestHeight}cm </p>
       </div>
     </MantineProvider>
   );
--- src/components/tallestUser.tsx
+++ src/components/tallestUser.tsx
@@ -10,3 +10,17 @@ import useUsers from './fetchData';
  * 
  * export tallestUser
  */
+
+export default function tallestHeight() {
+    const { users, isLoading } = useUsers();
+    const [tallestHeight, setTallestHeight] = useState(0);
+
+    useEffect(() => {
+        if (users.length > 0) {
+            const tallestUser = users.filter((user) => user.height === Math.max(...users.map((u) => u.height)));
+            setTallestHeight(tallestUser[0].height);
+        }
+
+    }, [users]);
+    return { tallestHeight };
+}
