c1a4af51532baa582a30088bb76e8b501df934e1
smolbebby <125963415+smolbebby@users.noreply.github.com>
2024-02-13 19:54:05 +1000

    updated file

--- src/components/averageAge.tsx
+++ src/components/averageAge.tsx
@@ -2,7 +2,7 @@
  * averageAge component calculates and returns the average age of all users from fetchData.ts.
  * @returns {{ averageAge }} Object containing the calculated average.
  */
-import { User } from "./useUsers";
+import User from "./useUsers";
 
 export default function averageAge(users: User[]) {
   const totalAge = users.reduce((sum, user) => sum + user.age, 0);
